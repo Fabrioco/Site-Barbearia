eb747c213954c517f53e824d487869c4e8911b79
Fabricio Lopes <fabriciooliveiralopes50@gmail.com>
2024-09-16 21:39:12 -0300

    :sparkles: feat: banco de dados firestore adicionado e configurado

---
+++ src/services/firebaseConnection.js
@@ -0,0 +1,15 @@
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAXMXIbBNoVyJ4QKWU4M5Y6dEeemlKjha4",
+  authDomain: "barbeiro-b928f.firebaseapp.com",
+  projectId: "barbeiro-b928f",
+  storageBucket: "barbeiro-b928f.appspot.com",
+  messagingSenderId: "286752950229",
+  appId: "1:286752950229:web:795a4afe66e03108da5f5c",
+  measurementId: "G-K4H9XYVYD9",
+};
+
+const app = initializeApp(firebaseConfig);
+export const db = getFirestore(app);
