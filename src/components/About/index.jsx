b5c88723ef2804c2e9c912210567fd13ab70d19c
Fabricio Lopes <fabriciooliveiralopes50@gmail.com>
2024-09-21 08:15:34 -0300

    :lipstick: feat: estlização da galeria

--- src/components/About/index.jsx
+++ src/components/About/index.jsx
@@ -1,5 +1,10 @@
 import React from "react";
-import demo from "../../assets/images/fotodemo.jpg";
+import corte1 from "../../assets/images/corte1.jpg";
+import corte2 from "../../assets/images/corte2.jpg";
+import corte3 from "../../assets/images/corte3.jpg";
+import corte4 from "../../assets/images/corte4.jpg";
+import corte5 from "../../assets/images/corte5.jpg";
+
 import "./about.css";
 
 export default function About() {
@@ -15,53 +20,53 @@ export default function About() {
       </p>
       <div className="gallery">
         <div className="post">
-          <img src={demo} alt="" />
+          <img src={corte1} alt="Corte masculino" />
           <div className="text-overlay">
-            <h3>foto 1</h3>
+            <h3>Corte Masculino</h3>
             <p>
               Lorem ipsum dolor sit amet, consectetur adipisicing elit. A fugit
-              debitis temporibus quos 
+              debitis temporibus quos
             </p>
           </div>
         </div>
-        <div className="gallery-four">
+        <div className="posts">
           <div className="post">
-            <img src={demo} alt="" />
+            <img src={corte2} alt="Corte Infantil" />
             <div className="text-overlay">
-              <h3>foto 1</h3>
+              <h3>Corte Infantil</h3>
               <p>
                 Lorem ipsum dolor sit amet, consectetur adipisicing elit. A
-                fugit debitis temporibus 
+                fugit debitis temporibus
               </p>
             </div>
           </div>
           <div className="post">
-            <img src={demo} alt="" />
+            <img src={corte3} alt="Barba" />
             <div className="text-overlay">
-              <h3>foto 1</h3>
+              <h3>Bigode</h3>
               <p>
                 Lorem ipsum dolor sit amet, consectetur adipisicing elit. A
-                fugit debitis temporibus 
+                fugit debitis temporibus
               </p>
             </div>
           </div>
           <div className="post">
-            <img src={demo} alt="" />
+            <img src={corte4} alt="Barba" />
             <div className="text-overlay">
-              <h3>foto 1</h3>
+              <h3>Barba</h3>
               <p>
                 Lorem ipsum dolor sit amet, consectetur adipisicing elit. A
-                fugit debitis temporibus 
+                fugit debitis temporibus
               </p>
             </div>
           </div>
           <div className="post">
-            <img src={demo} alt="" />
+            <img src={corte5} alt="Pintura de Cabelo" />
             <div className="text-overlay">
-              <h3>foto 1</h3>
+              <h3>Pintura de cabelo</h3>
               <p>
                 Lorem ipsum dolor sit amet, consectetur adipisicing elit. A
-                fugit debitis temporibus 
+                fugit debitis temporibus
               </p>
             </div>
           </div>
